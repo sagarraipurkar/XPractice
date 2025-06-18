4eb19a2f62c7cf780aa164e99fa3c54367acab8a
Sagar91-boop <er.sagarraipurkar@gmail.com>
2025-06-18 15:09:06 +0530

    Chnages in pom.xml file

--- src/test/java/test/Practice.java
+++ src/test/java/test/Practice.java
@@ -8,16 +8,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Practice {
+	WebDriver driver;
 
-	@Test (groups = {"smoke"})
+	@Test(groups = { "smoke" })
 	public void ClickOnSearchButtonWithSvg() {
+		String browser = System.getProperty("browser");
 		// Setup ChromeOptions
 		ChromeOptions option = new ChromeOptions();
+		FirefoxOptions fireOption = new FirefoxOptions();
+		EdgeOptions edgeOptions = new EdgeOptions();
 		option.addArguments("--incognito");
 		option.addArguments("--disable-notification");
 
@@ -29,7 +37,13 @@ public class Practice {
 		prefs.put("profile", profile);
 		option.setExperimentalOption("prefs", prefs);
 
-		WebDriver driver = new ChromeDriver(option);
+		if (browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver(option);
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver(fireOption);
+		}else if (browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver(edgeOptions);
+		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -47,5 +61,4 @@ public class Practice {
 		}
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("first_name"))).sendKeys("Sagar");
 	}
-
 }
